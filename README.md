b371d68f7fd5bf4292668a5094f1423e19cc0671
Harish <harish210430@gmail.com>
2024-10-22 00:07:01 -0700

    basic Js mcqs

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # JavaScript_Practice_Que
 Here we practice JS problems.
+Basic Declaration variable and typeof problems.
---
+++ mcqs.js
@@ -0,0 +1,13 @@
+// Practice MCQS on CodeChef.
+console.log("5 5"); // 5 5
+console.log("Js" + "program"); // Jsprogram
+let sequence = [1, 2, 3, 4];
+console.log(sequence); // Array: [1, 2, 3, 4]
+let x = "55.56", y = 88;
+console.log(x+y); // 55.5688
+console.log(typeof 1); // Number
+console.log(typeof 2.3); // Number
+console.log(typeof "3+2i"); // String
+console.log(t = 10 == 9); // false 
+let speed1 = 36;
+console.log(speed1 * (5/18));
